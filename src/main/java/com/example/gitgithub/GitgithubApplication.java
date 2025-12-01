4b4380743f71d8d4763d94309f1908fcc2abca76
derbe <derbely887@gmail.com>
2025-12-01 10:16:44 +0100

    first commit

---
+++ src/main/java/com/example/gitgithub/GitgithubApplication.java
@@ -0,0 +1,15 @@
+package com.example.gitgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgithubApplication.class, args);
+		file1 file = new file1();
+		System.out.println(file.getMessage());
+	}
+
+}
---
+++ src/main/java/com/example/gitgithub/file1.java
@@ -0,0 +1,9 @@
+package com.example.gitgithub;
+
+public class file1 {
+    private String message = "Hello, GitHub!";
+
+    public String getMessage() {
+        return message;
+    }
+}
